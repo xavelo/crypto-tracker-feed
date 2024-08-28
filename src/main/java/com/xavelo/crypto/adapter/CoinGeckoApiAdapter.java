a77b908798dc06eb17936730206637bc1e33865d
xavelo <xavelo@gmail.com>
2024-08-28 10:10:15 +0200

    logs

--- src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
+++ src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
@@ -41,51 +41,28 @@ public class CoinGeckoApiAdapter implements PriceService {
                 .header("accept", "application/json")
                 .asJson();
 
-        String bitcoinPrice = "0";
+        String coinPrice = "0";
         if (response.isSuccess()) {
             JsonNode jsonNode = response.getBody();
             System.out.println("Parsed JSON:");
             System.out.println(jsonNode);
 
             // Example of accessing a specific field
-            bitcoinPrice = String.valueOf(jsonNode.getObject().getJSONObject("bitcoin").getInt("usd"));
-            System.out.println("Bitcoin current price: " + bitcoinPrice);
+            coinPrice = String.valueOf(jsonNode.getObject().getJSONObject(coin.getFullName()).getInt(currency.getFullName().toLowerCase()));
+            logger.info("received {} price for {} coin", coinPrice, coin.getFullName());
+            System.out.println("Bitcoin current price: " + coinPrice);
         } else {
             System.out.println("Failed to retrieve data. Status code: " + response.getStatus());
         }
 
-        return new Price(coin, BigDecimal.valueOf(Long.valueOf(bitcoinPrice)), currency, Instant.now());
+        return new Price(coin, BigDecimal.valueOf(Long.valueOf(coinPrice)), currency, Instant.now());
 
     }
 
     private String getUrl(Coin coin, Currency currency) {
         String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
-        logger.debug("CoinGecko API URL {}", url);
+        logger.info("GET request to CoinGecko API URL {}", url);
         return url;
-    }
-
-
-    public Price test(Coin coin, Currency currency) throws PriceFetchException {
-        try {
-            String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
-            logger.debug("CoinGecko API URL {}", url);
-
-
-            CoinGeckoResponse response = restTemplate.getForObject(url, CoinGeckoResponse.class);
-            BigDecimal price = null;
-            if (response != null) {
-                price = response.getCurrentPrice();
-            } else {
-                throw new PriceFetchException("Unable to fetch price from CoinGecko API");
-            }
-
-            logger.debug("CoinGeckoResponse response {}", response);
-            return new Price(coin, price, currency, Instant.now());
-        } catch (Exception e) {
-            String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
-            logger.debug("CoinGecko API URL {}", url);
-            throw new PriceFetchException("Error fetching price from CoinGecko API", e);
-        }
     }
 
 }
