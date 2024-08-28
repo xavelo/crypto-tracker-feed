08f9c91880b06a8ad20a2847093f8861725fd0a3
xavelo <xavelo@gmail.com>
2024-08-28 16:04:21 +0200

    fetchAndPublishPrice

--- src/main/java/com/xavelo/crypto/service/PublishServiceImpl.java
+++ src/main/java/com/xavelo/crypto/service/PublishServiceImpl.java
@@ -5,14 +5,20 @@ import com.xavelo.crypto.adapter.KafkaAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 public class PublishServiceImpl implements PublishService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PublishServiceImpl.class);
 
     @Autowired
     KafkaAdapter kafkaAdapter;
 
     @Override
     public void publishPrice(Price price) {
-        kafkaAdapter.produceMessage("test-topic", "test");
+        logger.info("publishing price update for {}: {}{}", price.getCoin(), price.getPrice(), price.getCurrency());
+        kafkaAdapter.produceMessage("test-topic", price.getCoin() + " " + price.getPrice() + "  " + price.getCurrency());
     }
 }
