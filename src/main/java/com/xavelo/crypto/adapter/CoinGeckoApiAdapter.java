c98eaee162b6b94e8e3c0d237edfe469394df69e
xavelo <xavelo@gmail.com>
2024-08-28 09:07:47 +0200

    unirest

--- src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
+++ src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
@@ -13,6 +13,10 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
 @Component
 public class CoinGeckoApiAdapter implements PriceService {
 
@@ -30,7 +34,38 @@ public class CoinGeckoApiAdapter implements PriceService {
     }
 
     @Override
-    public Price fetchPrice(Coin coin, Currency currency) throws PriceFetchException {
+    public Price fetchPrice(Coin coin, Currency currency) {
+
+        String url = getUrl(coin, currency);
+        HttpResponse<JsonNode> response = Unirest.get(url)
+                .header("accept", "application/json")
+                .asJson();
+
+        String bitcoinPrice = "0";
+        if (response.isSuccess()) {
+            JsonNode jsonNode = response.getBody();
+            System.out.println("Parsed JSON:");
+            System.out.println(jsonNode);
+
+            // Example of accessing a specific field
+            bitcoinPrice = jsonNode.getObject().getJSONArray("0").getString(4);
+            System.out.println("Bitcoin current price: " + bitcoinPrice);
+        } else {
+            System.out.println("Failed to retrieve data. Status code: " + response.getStatus());
+        }
+
+        return new Price(coin, BigDecimal.valueOf(Long.valueOf(bitcoinPrice)), currency, Instant.now());
+
+    }
+
+    private String getUrl(Coin coin, Currency currency) {
+        String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
+        logger.debug("CoinGecko API URL {}", url);
+        return url;
+    }
+
+
+    public Price test(Coin coin, Currency currency) throws PriceFetchException {
         try {
             String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
             logger.debug("CoinGecko API URL {}", url);
