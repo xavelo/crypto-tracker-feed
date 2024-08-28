9e6f1face868036a319824217fc10ab56e4507df
xavelo <xavelo@gmail.com>
2024-08-28 12:42:04 +0200

    fix currency symbol

--- src/main/java/com/xavelo/crypto/Currency.java
+++ src/main/java/com/xavelo/crypto/Currency.java
@@ -2,15 +2,20 @@ package com.xavelo.crypto;
 
 public enum Currency {
 
-    USD("US Dollar"),
-    EUR("Euro");
+    USD("USD", "US Dollar"),
+    EUR("EUR", "Euro");
 
+    private final String symbol;
     private final String fullName;
 
-    Currency(String fullName) {
+    Currency(String symbol, String fullName) {
+        this.symbol = symbol;
         this.fullName = fullName;
     }
 
+    public String getSymbol() {
+        return symbol;
+    }
     public String getFullName() {
         return fullName;
     }
--- src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
+++ src/main/java/com/xavelo/crypto/adapter/CoinGeckoApiAdapter.java
@@ -60,8 +60,9 @@ public class CoinGeckoApiAdapter implements PriceService {
     }
 
     private String getUrl(Coin coin, Currency currency) {
-        String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.name().toLowerCase(), API_KEY);
+        String url = String.format("%s/simple/price?ids=%s&vs_currencies=%s&x_cg_demo_api_key=%s", apiUrl, coin.getFullName(), currency.getSymbol().toLowerCase(), API_KEY);
         logger.info("GET request to CoinGecko API URL {}", url);
+        System.out.println("GET request to CoinGecko API URL " + url);
         return url;
     }
 
