eb61dccb6846685423ed4b71b41e1fe3c3bcc903
= <=>
2024-09-21 21:37:02 +0200

    update cron

--- src/main/java/com/xavelo/crypto/cron/ScheduledPriceUpdater.java
+++ src/main/java/com/xavelo/crypto/cron/ScheduledPriceUpdater.java
@@ -5,13 +5,18 @@ import com.xavelo.crypto.Coin;
 import com.xavelo.crypto.Currency;
 import com.xavelo.crypto.adapter.PriceFetchException;
 import com.xavelo.crypto.service.FetchService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduledPriceUpdater {
 
-    private final FetchService fetchService;
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledPriceUpdater.class);
+
+    private final FetchService fetchService;    
 
     public ScheduledPriceUpdater(FetchService fetchService) {
         this.fetchService = fetchService;
@@ -19,9 +24,11 @@ public class ScheduledPriceUpdater {
 
     @Scheduled(fixedRate = 30000) // Run every 30 seconds
     public void scheduledPriceUpdate() throws PriceFetchException, JsonProcessingException {
-        System.out.println("scheduledPriceUpdate...");
+        logger.info("-----> scheduledPriceUpdate STARTED");
         fetchService.fetchAndPublishPrice(Coin.BTC, Currency.USD);
         fetchService.fetchAndPublishPrice(Coin.ETH, Currency.USD);
+        fetchService.fetchAndPublishPrice(Coin.XRP, Currency.USD);
+        logger.info("<----- scheduledPriceUpdate DONE");
     }
 
 }
